e911dc0b59665020f33aa7689da91918a914b1bc
Yekinni Ajibola <yekinnijibola@gmail.com>
2021-11-06 20:32:48 +0100

    fixed self signed bug

--- src/database/config.js
+++ src/database/config.js
@@ -1,7 +1,12 @@
 const Sequelize = require("sequelize");
 const connectionString = process.env.DATABASE_URL;
 const sequelize = new Sequelize(connectionString, {
-  dialectOptions: { ssl: true },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
